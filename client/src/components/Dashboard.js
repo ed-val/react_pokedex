9bcfbeaca2f13e8e89f5750b808e94843b74fb44
ed-val2 <e.valbuena.d@gmail.com>
2020-05-17 18:23:41 -0500

    Added logic to render and layout items

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,48 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PokemonPreview from './pokemonPreview'
 import { getPokedex, getPokemonTypes } from '../actions';
 
-import { Link } from 'react-router-dom';
-const imageContainerStyle = { 
-  backgroundColor: "#eeeeee", 
-  display: "flex", 
-  alignItems: "center", 
-  justifyContent: "center", 
-  padding: "5px" ,
-  borderRadius: "6px"
-};
-const Dashboard = (props) => {
-  const onBtnClick = () => {
-    props.getPokedex('kanto');
-    props.getPokemonTypes('kanto');
+class Pokedex extends Component {
+  constructor(props) {
+    super(props);
+    this.props.getPokedex();
+    this.props.getPokemonTypes();
+    this.state = {};
   }
 
-  return (
-    <div className="row s12">
-      <button onClick={() => onBtnClick()} className="waves-effect waves-light btn-small red darken-4">Go!</button>
-      <div className="col s3" style={{marginTop: "15px", marginBottom: "15px"}}>
-        <div style={imageContainerStyle} >
-          <img 
-            style={{ height:"75%", width:"75%" }} 
-            className="waves-effect waves-light"
-            src="https://pokeres.bastionbot.org/images/pokemon/1.png"
-          >
-          </img>
+  onBtnClick() {
+    const {getPokedex, pokemons, nextBatch} = this.props;
+    getPokedex(nextBatch, pokemons);
+  }
+
+  renderPreviews(pokemons = []) {
+    return pokemons.map(pokemon => {
+      return (
+        <PokemonPreview 
+          key={pokemon.name.toString()}
+          name={pokemon.name}
+          id={pokemon.id}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="col s12" style={{ paddingLeft: "15px", paddingRight: "15px" }}>
+        <div className="right-align grey lighten-1" style={{ paddingRight: '20px', borderRadius: '50px' }}>
+          <h2 className="white-text">Pokedex</h2>
         </div>
-        <div className="card-content">
-          <h5 className="grey-text text-darken-2">Bulbasaur</h5>
-          <h6 className="black-text bold">No. 001</h6>
-        </div>
+        {this.props.pokemons.length > 0 &&
+          <div className="col s12">
+            <div className="row s12">
+              {this.renderPreviews(this.props.pokemons)}
+            </div>
+            <div className="col s12 center-align" style={{margin: '20px'}}>
+              <button 
+                className="waves-effect waves-light btn-large red darken-4"
+                onClick={this.onBtnClick.bind(this)}
+              >
+                Search More Pokemons!
+              </button>
+            </div>
+          </div>
+        }
       </div>
-
-    </div>
-  );
-};
-const mapStateToProps = ({ auth, header }) => {
-  return {
-
-  };
+    );
+  }
 };
 
-export default connect(mapStateToProps, { getPokedex, getPokemonTypes })(Dashboard);
+const mapStateToProps = ({ pokedex }) => ({
+  pokemons: pokedex.pokemons,
+  nextBatch: pokedex.nextBatch,
+});
+
+export default connect(mapStateToProps, { getPokedex, getPokemonTypes })(Pokedex);
 
