22ceda4690207cdf33495efca3be05957b1d0b3c
ed-val2 <e.valbuena.d@gmail.com>
2020-05-18 09:01:56 -0500

    added new stores

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,14 @@ import { reducer as reduxForm } from 'redux-form'; //<- this syntax is really us
 import surveysReducer from './surveysReducer';
 import authReducer from './authReducer';
 import headerReducer from './headerReducer';
+import pokedexReducer from './pokedexReducer';
+import pokemonReducer from './pokemonReducer';
 
 export default combineReducers({
   auth: authReducer,
   header: headerReducer,
   form: reduxForm,
   surveys: surveysReducer,
+  pokedex: pokedexReducer,
+  pokemon: pokemonReducer,
 });
