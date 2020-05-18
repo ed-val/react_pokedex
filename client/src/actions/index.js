9a593811514c6d1269fe21868baaf9721c26505c
ed-val2 <e.valbuena.d@gmail.com>
2020-05-18 08:56:40 -0500

    new actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,85 +24,102 @@ export const handleToken = (token) => async dispatch => {
   }
 };
 
-export const submitSurvey = (values, history) => async dispatch => {
-  dispatch( { type: 'POST_SURVEY' });
-  try {
-    const res = await axios.post('/api/surveys', values);
-    if (res) dispatch({ type: 'FETCH_USER_SUCCESS', payload: res.data });
-    if (!res) dispatch({ type: 'FETCH_USER_FAIL'});
-    history.push('/surveys');
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const fetchSurveys = () => async dispatch => {
-  dispatch({ type: 'FETCH_SURVEYS' });
-  try {
-    const res = await axios.get('/api/surveys');
-    if (res) dispatch({ type: 'FETCH_SURVEYS_SUCCESS', payload: res.data });
-    if (!res) dispatch({ type: 'FETCH_SURVEYS_FAIL'});
-  } catch (e) {
-    console.log(e);
-    dispatch({ type: 'FETCH_SURVEYS_FAIL', payload: e });
-  }
-
-};
-
 export const searchBarInputChange = (input) => {
   return { type: 'CHANGE_USER_SB_INPUT', payload: input };
 };
 
-export const getPokedex = (id) => {
-  return async (dispatch) => {
-    dispatch({
-      type: 'GET_POKEDEX'
-    });
-
-    try {
-      const responseLogin = await API.getPokedex(id);
-
-      // if (response.error || responseLogin.error || responseRegisterDevice.error) {
-      //   dispatch({
-      //     type: 'GET_AUTH_DATA_FAIL',
-      //     payload: response.errorMsg
-      //   });
-      //   return; // salir de la accion
-      // }
-
-      // dispatch({
-      //   type: 'GET_AUTH_DATA_SUCCESSFUL',
-      //   payload: responseLogin.token
-      // });
-
-      // dispatch({
-      //   type: 'GET_USER_DATA_SUCCESSFUL',
-      //   payload: response.data
-      // });
-
-      // NavigationService.navigate('Form'); // go go go
-    } catch (e) {
-      console.log(e);
-      dispatch({
-        type: 'GET_POKEDEX_FAIL'
-      });
-    }
-  };
+export const updatePokemonSelected = (numberID) => {
+  return { type: 'UPDATE_POKEMON_SELECTED', payload: numberID };
 };
 
-export const getPokemonTypes = (id) => {
-  return async (dispatch) => {
+export const landingIsActive = (value) => {
+  return { type: 'LANDING_IS_ACTIVE', payload: value };
+};
+
+export const getPokedex = (nextBatch, pokemons) => async dispatch => {
+  dispatch({
+    type: 'GET_POKEDEX'
+  });
+
+  try {
+    const response = await API.getPokedex(nextBatch, pokemons);
+
+    if (response.error) {
+      dispatch({
+        type: 'GET_POKEDEX_DATA_FAIL',
+        payload: response.errorMsg
+      });
+      return; // salir de la accion
+    }
+
     dispatch({
-      type: 'GET_POKEMON_TYPES'
+      type: 'GET_POKEDEX_DATA_SUCCESSFUL',
+      payload: { previews: response.previews, nextBatch: response.nextBatch }
     });
 
-    try {
-      const responseLogin = await API.getTypes(id);
-    } catch (e) {
-      console.log(e);
+  } catch (e) {
+    console.log(e);
+    dispatch({
+      type: 'GET_POKEDEX_DATA_FAIL',
+    });
+  }
+};
+
+export const getPokemonTypes = () => async dispatch => {
+  dispatch({
+    type: 'GET_POKEMON_TYPES'
+  });
+
+  try {
+    const response = await API.getTypes();
+    console.log(response)
+    if (response.error) {
       dispatch({
-        type: 'GET_POKEMON_TYPES_FAIL'
+        type: 'GET_POKEMON_TYPES_FAIL',
+        payload: response.errorMsg
       });
+      return; // salir de la accion
     }
-  };
+  
+    dispatch({
+      type: 'GET_POKEMON_TYPES_SUCCESSFUL',
+      payload: response.types
+    });
+
+  } catch (e) {
+    console.log(e);
+    dispatch({
+      type: 'GET_POKEMON_TYPES_FAIL'
+    });
+  }
+};
+
+export const getPokemon = (id) => async dispatch => {
+  console.log(id);
+  dispatch({
+    type: 'GET_POKEMON'
+  });
+
+  try {
+    const response = await API.getPokemon(id);
+
+    if (response.error) {
+      dispatch({
+        type: 'GET_POKEMON_FAIL',
+        payload: response.errorMsg
+      });
+      return; // salir de la accion
+    }
+  
+    dispatch({
+      type: 'GET_POKEMON_SUCCESSFUL',
+      payload: response.pokemon
+    });
+
+  } catch (e) {
+    console.log(e);
+    dispatch({
+      type: 'GET_POKEMON_FAIL'
+    });
+  }
 };
