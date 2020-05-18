9bf249f8c5525179795320a30e7f3a120109271b
ed-val2 <e.valbuena.d@gmail.com>
2020-05-18 08:58:56 -0500

    moved searchbar out of component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { searchBarInputChange } from '../actions';
-import Payments from './Payments';
+import { searchBarInputChange, getPokemon } from '../actions';
+// import Payments from './Payments';
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showSearchBar: false,
+      searchbarText: '',
     };
+  }
+
+  _renderContent() {
+    switch (this.props.user) {
+      case null:
+        return;
+      case false:
+        // return <li onClick={this.props.landingIsActive(false)}><a href="/auth/google">Login with Google</a></li>;
+      default:
+        return [
+          // <li key="1"><Payments /></li>,
+          // <li key="2" onClick={this._toggleSearchBar.bind(this)} ><a><i className="material-icons left">search</i>Search Pokemon</a></li>,
+          // <li key="2" style={{ margin: '0 30px' }}>Credits: {this.props.user.credits}</li>,
+          <li key="3"><a href="/api/logout">Log Out</a></li>
+        ];
+    }
   }
 
   _toggleSearchBar() {
@@ -17,32 +34,21 @@ class Header extends Component {
   }
   
   _onSearchBarChange(event) {
-    this.props.searchBarInputChange(event.target.value);
+    this.setState({ searchbarText: event.target.value});
   }
 
-  _renderContent() {
-    console.log(this.props.user);
-    switch (this.props.user) {
-      case null:
-        return;
-      case false:
-        return <li><a href="/auth/google">Login with Google</a></li>;
-      default:
-        return [
-          // <li key="1"><Payments /></li>,
-          <li key="2" onClick={this._toggleSearchBar.bind(this)} ><a><i className="material-icons left">search</i>Search Pokemon</a></li>,
-          // <li key="2" style={{ margin: '0 30px' }}>Credits: {this.props.user.credits}</li>,
-          <li key="3"><a href="/api/logout">Log Out</a></li>
-        ];
+  _handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      this.props.getPokemon(this.state.searchbarText);
     }
   }
 
   renderSearchBar() {
     return (
-      <div className="red darken-4 nav-wrapper">
+      <div className="row s12 red darken-4 nav-wrapper">
         <form>
           <div className="input-field">
-            <input onChange={this._onSearchBarChange.bind(this)} id="search" type="search" required></input>
+            <input onKeyDown={this._handleKeyDown.bind(this)} placeholder='Search by ID or name' onChange={this._onSearchBarChange.bind(this)} id="search" type="search" required></input>
             <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
             <i onClick={this._toggleSearchBar.bind(this)} className="material-icons">close</i>
           </div>
@@ -92,4 +98,7 @@ const mapStateToProps = ({ auth, header }) => {
   };
 };
 
-export default connect(mapStateToProps, {searchBarInputChange})(Header);
+export default connect(mapStateToProps, {
+  searchBarInputChange,
+  getPokemon,
+})(Header);
